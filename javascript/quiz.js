db7c2589c44b88938618cfd9befe65cae8af2241
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-05-21 14:05:19 +0200

    Start på quiz

---
+++ javascript/quiz.js
@@ -0,0 +1,23 @@
+'use strict';
+const quiz = [
+  {
+question: "Hvor mange procent % af unge i Danmark <br> har haft en Angst lidelse inden de bliver 18?",
+    options: ["20%", "13%", "15%", "17%"],
+    answer: 2
+  },
+  {
+    question: "Hvilket af følgende symptomer er IKKE <br> et muligt tegn på et angstanfald?",
+    options: ["Hjertebanken", "Feber", "Kvalme", "Sortnen for øjnene"],
+    answer: 1
+  },
+  {
+    question: "Hvad kan et slemt angstanfald føles som?",
+    options: ["En allergisk reaktion", "En madforgiftning", "En influenza", "Et hjerteanfald"],
+    answer: 3
+  },
+  {
+    question: "Hvad er det bedste, du kan gøre for en ven under et angstanfald?",
+    options: ["Være til stede og spørge hvad de har brug for", "Give dem et kram", "Tage dem med ud i frisk luft uden at spørge først", "Fortælle dem hvad de skal gøre"],
+    answer: 0
+  }
+];
