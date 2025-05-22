57ad0ed95fa6d997352b3a3f9a4339baf8383d35
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-05-22 19:08:32 +0200

    404 screen
    depression 404 side on click

--- javascript/redirect.js
+++ javascript/redirect.js
@@ -1,14 +1,25 @@
 
   let adhdReady = false; // Ændres hvis adhd underside er klar til brug 
-  let depressionReady = false; // Ændres hvis depression underside er klar til brug
+  let depressionReady = true; // Ændres hvis depression underside er klar til brug
   
   document.getElementById("klik-gif1").onclick = function() {
     window.location.href = 'angst-video.html';
   };
 
   document.getElementById("klik-gif2").onclick = function() {
-    window.location.href = 'angst-video.html';
-  }
+    if (depressionReady == true) {
+        window.location.href = 'depression-video.html';
+        console.log("videoen er klar");
+    }
+    else {
+        const notifikation = document.getElementById('error');
+        notifikation.textContent = "Videoen kunne desværre ikke afspilles! Vi beklager.";
+        notifikation.classList.add('error-vist');
+        
+        setTimeout(function() { //notifikationen forsvinder efter 3sek
+            notifikation.classList.remove('error-vist');
+        }, 3000);
+    }};
 
   document.getElementById("klik-gif3").onclick = function() {
     if (adhdReady == true) {
@@ -16,14 +27,11 @@
         console.log("videoen er klar");
     }
     else {
-        console.log("notifikation vist") 
         const notifikation = document.getElementById('error');
         notifikation.textContent = "Videoen kunne desværre ikke afspilles! Vi beklager.";
         notifikation.classList.add('error-vist');
         
-        setTimeout(function() {
+        setTimeout(function() { //notifikationen forsvinder efter 3sek
             notifikation.classList.remove('error-vist');
         }, 3000);
     }};
-
-
